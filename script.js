c4f4b64e15203df1d38d24efa90b3e9a022ff9c8
Gradels <302negag@student.dist113.org>
2026-09-11 10:24:22 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+document.getElementById("test_select").addEventListener("submit", function() {
+  var input = document.getElementById("select").value
+  if (input == "precalc") {
+    // do something
+  } else if (input == "calc") {
+    // something else idk
+  } else if (input == "mulitv") {
+    // you guessed it
+  } else {
+    document.body.remove()
+  }
+})
